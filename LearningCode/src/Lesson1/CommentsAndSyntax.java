ef869a5f8295fa90d190983a11f5b422ce0c360d
James Sinclair <jsincl4ir@gmail.com>
2024-05-27 15:29:23 +0200

    Adding the initial project and first lesson.

---
+++ LearningCode/src/Lesson1/CommentsAndSyntax.java
@@ -0,0 +1,12 @@
+package Lesson1;
+
+/* This is a comment */
+public class CommentsAndSyntax {
+    // This is also a comment
+    public static void main(String[] args) {
+        // The line below this is code that has been commented out. Try to un-comment it, and run this java program.
+        //System.out.println("I removed the comments!");
+
+        // Add a new line after this comment, and add a new comment of your own. You can use either comment style.
+    }
+}
