6e5f0e056563d1c44ac328e2cdbf7356c964de75
CarliSinclair <c.sincl4ir@gmail.com>
2024-11-14 21:55:33 +0200

    my first commit

--- LearningCode/src/Lesson1/CommentsAndSyntax.java
+++ LearningCode/src/Lesson1/CommentsAndSyntax.java
@@ -5,8 +5,10 @@ public class CommentsAndSyntax {
     // This is also a comment
     public static void main(String[] args) {
         // The line below this is code that has been commented out. Try to un-comment it, and run this java program.
-        //System.out.println("I removed the comments!");
+        System.out.println("I removed the comments!");
+        System.out.println("This is my first code");
 
         // Add a new line after this comment, and add a new comment of your own. You can use either comment style.
+        // This is hard
     }
 }
